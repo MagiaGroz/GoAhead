04211b3cf4981406bd7c459e93d730c69ec8f50d
BolatD <47204521+BolatD@users.noreply.github.com>
2020-04-20 02:18:41 +0600

    courses-detail

--- goahead-front/src/app/courses-list/courses-list.component.ts
+++ goahead-front/src/app/courses-list/courses-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import {BasicService} from "../basic.service";
+import {Course} from "../models";
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.css']
 })
 export class CoursesListComponent implements OnInit {
+  courses: Course[]=[];
 
-  constructor() { }
+  constructor(public basicService:BasicService) { }
 
   ngOnInit(): void {
+    this.getCourseList();
+  }
+  getCourseList(){
+    this.basicService.getCourseList().subscribe(courses =>{
+      this.courses=courses
+    });
   }
 
+  deleteCourse(id) {
+    this.basicService.deleteCourse(id).subscribe(res => {
+       this.courses = this.courses.filter(c => c.id != id);
+       this.getCourseList();
+    });
+}
 }
