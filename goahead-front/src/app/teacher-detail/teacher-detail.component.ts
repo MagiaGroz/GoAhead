5f2f9fbbdd248d98e919ddb30d1b3aa0bf06f1de
MagiaGroz <grozaga@gmail.com>
2020-04-20 02:38:16 +0600

    teacher detail completed

--- goahead-front/src/app/teacher-detail/teacher-detail.component.ts
+++ goahead-front/src/app/teacher-detail/teacher-detail.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {Course, Teacher} from '../models';
+import {BasicService} from '../basic.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-teacher-detail',
-  templateUrl: './teacher-detail.component.html',
-  styleUrls: ['./teacher-detail.component.css']
+  selector: 'app-teachers-list',
+  templateUrl: './teachers-list.component.html',
+  styleUrls: ['./teachers-list.component.css']
 })
-export class TeacherDetailComponent implements OnInit {
+export class TeachersListComponent implements OnInit {
+  teachers: Teacher[]=[];
+  courseId:string;
 
-  constructor() { }
+  constructor(private basicService: BasicService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getTeachersByCourse(this.courseId);
   }
-
+  
+  getTeachersByCourse(id:string){
+    this.basicService.getTeachersByCourse(id).subscribe(teachers =>{
+      this.teachers=teachers;
+    })
+  }
 }
