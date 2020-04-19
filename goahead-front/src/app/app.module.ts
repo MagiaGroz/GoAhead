05982bcf2eb23607400cf94d1b3107d276f7f20f
BolatD <47204521+BolatD@users.noreply.github.com>
2020-04-20 01:53:59 +0600

    components

--- goahead-front/src/app/app.module.ts
+++ goahead-front/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
+import { TeacherDetailComponent } from './teacher-detail/teacher-detail.component';
+import { TeachersListComponent } from './teachers-list/teachers-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { MyCoursesComponent } from './my-courses/my-courses.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseDetailComponent,
+    CoursesListComponent,
+    TeacherDetailComponent,
+    TeachersListComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    LoginComponent,
+    RegistrationComponent,
+    MyCoursesComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
