e90b9100d2a6a6ea18dc9592ba0c90141592d65f
Ameer <mynbay01@gmail.com>
2020-04-25 13:28:01 +0600

    changed main page

--- goahead-front/src/app/app-routing.module.ts
+++ goahead-front/src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
         children: [
             {
                 path: '',
-                component: CoursesListComponent
+                component: LoginComponent
             },
             {
                 path: 'login',
