ae8b6c1fedb04b5d0678ee0f8096c51970aa41bd
Ameer <mynbay01@gmail.com>
2020-04-26 15:30:06 +0600

    header

--- goahead-front/src/app/header/header.component.ts
+++ goahead-front/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { BasicService } from "../basic.service";
+import { Course, Teacher, University } from '../models';
 
 @Component({
   selector: "app-header",
@@ -8,12 +9,15 @@ import { BasicService } from "../basic.service";
   styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
+
   displayDropdownWindows = {
     courses: false,
     teachers: false,
     universities: false,
   };
-
+  public courseList: Course[]=[];
+  public teacherList: Teacher[]=[];
+  public uniList: University[]=[];
   dropdownMenuDisplay(name) {
     if (name === "courses") this.displayDropdownWindows.courses = true;
     else if (name === "teachers") this.displayDropdownWindows.teachers = true;
@@ -27,13 +31,30 @@ export class HeaderComponent implements OnInit {
     else if (name === "universities")
       this.displayDropdownWindows.universities = false;
   }
-
   constructor(private router: Router, private basicService: BasicService) {}
   @Input()
   public isLoggedIn;
   @Input()
   public isSuperUser;
-  ngOnInit(): void {}
+  ngOnInit(): void {
+      this.basicService.getUniversityList().subscribe(res=>{
+          this.uniList = res
+      });
+      this.basicService.getCourseList().subscribe(res=>{ this.courseList = res});
+      this.basicService.getTeacherList().subscribe(res=>{this.teacherList = res});
+  }
+  courseItemClick(course: Course){
+    this.basicService.setCourseForDetailedView(course);
+      this.router.navigateByUrl('course-detail');
+  }
+  teacherItemClick(teacher: Teacher){
+      this.basicService.setTeacherForDetailedView(teacher);
+      this.router.navigateByUrl('teacher-detail')
+  }
+  unItemClick(university: University){
+      this.basicService.setUniversityForDetailView(university);
+      this.router.navigateByUrl('university-detail');
+  }
   signup() {
     this.router.navigateByUrl("signup").then();
   }
